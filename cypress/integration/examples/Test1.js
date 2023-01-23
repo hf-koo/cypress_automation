3b79c2c64f4e6725bb90db4e8040aa6ad77adb7c
HungFai Koo <jhk888@yahoo.com>
2023-01-22 22:21:22 -0800

    reuseable class name as Locator

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -10,8 +10,9 @@ describe("My first test suite", function () {
     //Parent child chaining
     cy.get(".products").find(".product").should("have.length", 4);
     cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
-
-    cy.get(".products")
+    //reuseable class as Locator
+    cy.get(".products").as("productLocator");
+    cy.get("@productLocator")
       .find(".product")
       .each(($el, index, $list) => {
         const textVeg = $el.find("h4.product-name").text();
@@ -19,5 +20,15 @@ describe("My first test suite", function () {
           cy.wrap($el).find("button").click();
         }
       });
+    // assert if log text is correctly displayed
+    cy.get(".brand").should("have.text", "GREENKART");
+
+    //this is to print in logs
+    const logo = cy.get(".brand").then(function (logoelement) {
+      cy.log(logoelement.text());
+    });
+    //const logo=cy.get('.brand')
+    //cy.log(cy.get('.brand').text())
+    // cy.log(logo.text())
   });
 });
