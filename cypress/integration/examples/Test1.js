60638537b44e689539bec006465a9cb3d0a061da
HungFai Koo <jhk888@yahoo.com>
2023-01-22 20:45:44 -0800

    using different method-find-type-text-click

--- cypress/integration/examples/Test1.js
+++ cypress/integration/examples/Test1.js
@@ -1,5 +1,23 @@
+/// <reference types="Cypress" />
+
 describe("My first test suite", function () {
   it("My firstTest case", function () {
     cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
+    cy.get(".search-keyword").type("ca");
+    cy.wait(2000);
+    //cypress get acts like find Element
+    cy.get(".product:visible").should("have.length", 4);
+    //Parent child chaining
+    cy.get(".products").find(".product").should("have.length", 4);
+    cy.get(".products").find(".product").eq(1).contains("ADD TO CART").click();
+
+    cy.get(".products")
+      .find(".product")
+      .each(($el, index, $list) => {
+        const textVeg = $el.find("h4.product-name").text();
+        if (textVeg.includes("Cashews")) {
+          cy.wrap($el).find("button").click();
+        }
+      });
   });
 });
